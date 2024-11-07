a1944abadeb3d435928611450e2dfcac4000a9a4
Rosario Pio Gnazzo <164646675+rosariopiognazzo@users.noreply.github.com>
2024-11-07 18:49:27 +0100

    Update Analisi-Univariate-Dataset.R

--- Analisi-Univariate-Dataset.R
+++ Analisi-Univariate-Dataset.R
@@ -15,18 +15,21 @@ str(dataset)
 #si nota che il formato degli elementi non è corretta per la maggior parte
 
 #aggiustiamo il formato delle variabili
-dataset$score <- as.numeric(dataset$score)
-dataset$is_retweet <- as.factor(dataset$is_retweet)
-dataset$is_quote_status <- as.factor(dataset$is_quote_status)
-dataset$sentiment <- as.factor(dataset$sentiment)
+dataset <- dataset %>%
+  mutate(is_retweet = as.factor(is_retweet),
+         is_quote_status = as.factor(is_quote_status),
+         sentiment = as.factor(sentiment),
+         score = as.numeric(score))
+
+write_csv2(dataset, "dataset.csv")#salvato nuovo dataset
+
+## questa conversione dobbiamo capire se farla a priori o solo quando usiamo le istanze della variabile
 # sostituisco le virgolette singole con doppie virgolette
 dataset$hashtags <- gsub("'", '"', dataset$hashtags)
 # sostituisco stringhe vuote con "[]" per evitare errori di parsing
 dataset$hashtags[dataset$hashtags == ""] <- "[]"
 # convertirto ogni elemento in JSON per poter accederci in maniera più immediata
 dataset$hashtags <- lapply(dataset$hashtags, fromJSON)
-
-write_csv2(dataset, "dataset.csv")#salvato nuovo dataset
 
 
 
